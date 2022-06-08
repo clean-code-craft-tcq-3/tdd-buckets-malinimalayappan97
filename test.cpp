8372f91fbb7f2356bed313e1d7cdfa45e0485abf
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-06-09 00:17:11 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "main.h"
 
 TEST_CASE("detect set of values  from  specified range within input array of integers") {
    int sampleArray[] ={3, 3, 5, 4, 10, 11, 12};
