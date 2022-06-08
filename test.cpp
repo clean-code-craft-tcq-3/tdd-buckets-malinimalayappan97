ae1a671e1c0eef3f185328556d48cebafb438920
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-06-08 23:30:15 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,3 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+
+TEST_CASE("detect range of input array of integers") {
+   REQUIRE(inferBreach(3,5) == 4);
+}
