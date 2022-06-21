d150eaf78bd823a9304c202086d28f4b88721a4b
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-06-21 22:38:58 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,13 @@
 
 TEST_CASE("detect set of values  from  specified range within input array of integers") {
    int sampleArray[] ={2,3, 3, 5, 4, 10, 11, 12};
-   int startRangeValue = 3;
+   
+   int startRangeValue = 2;
    int endRangeValue = 5;
-   REQUIRE(getNoOfRValuesInRange(sampleArray,startRangeValue,endRangeValue) == 4);
+   REQUIRE(getNoOfRValuesInRange(sampleArray,startRangeValue,endRangeValue) == 5);
+   
+   startRangeValue = 10;
+   endRangeValue = 12;
+   REQUIRE(getNoOfRValuesInRange(sampleArray,startRangeValue,endRangeValue) == 3);
+   
 }
