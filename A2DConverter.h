4be8589c1b96f03a45e3dfc0670b86c489e41f0b
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 19:30:54 +0530

    Update A2DConverter.h

--- A2DConverter.h
+++ A2DConverter.h
@@ -4,7 +4,7 @@
 #include "math.h"
 #include "CheckRange.h"
 
-int getA2DInRangeValue(int* sensorData,int inputBit ,int startRange, int endRange);
+int getA2DInRangeValue(int* sensorData,int inputBit ,int startRange, int endRange,int noOfValues);
 int ConvertToCurrentRange(int bitInput ,int*sensorData);
 bool  getActualCurrentvalue_12A2D(int*sensorData,int * currentInAmps);
 bool getActualChargeOrDischargeCurrentVaule_10A2D(int*sensorData,int * currentInAmps);
