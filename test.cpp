2bd276546d0df787b653e99241305e2cce4dbdff
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 19:31:17 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ TEST_CASE("detect set of values  from  specified range within input array of int
    int sampleArray[] ={1146,1147,4093,4092,2000};
    int startRangeValue = 2;
    int endRangeValue = 5;
-   getA2DInRangeValue(sampleArray,12,startRangeValue,endRangeValue);
+   getA2DInRangeValue(sampleArray,12,startRangeValue,endRangeValue,5);
 //    REQUIRE( == 5);
    
 //    startRangeValue = 10;
