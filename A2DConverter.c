4405f2d61de7763bb5b0d015d9f4c2f69055be6d
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 18:48:44 +0530

    Update A2DConverter.c

--- A2DConverter.c
+++ A2DConverter.c
@@ -1,1 +1,70 @@
+#include "A2DConverter.h"
 
+bool getActualChargeOrDischargeCurrentVaule_10A2D(int*sensorData,int * currentInAmps)
+{
+    float currentValue = 0.0;
+    int CurrentRoundedValue = 0;
+    
+    if((*sensorData < 1024)&&(*sensorData >=0))
+    {
+        currentValue = (30  *( *sensorData / 1023));
+        CurrentRoundedValue = round(currentValue);
+        *currentInAmps = abs(CurrentRoundedValue - 1023);
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+bool  getActualCurrentvalue_12A2D(int*sensorData,int * currentInAmps)
+{
+    if((*sensorData < 4095) &&(*sensorData >=0))
+    {
+        *currentInAmps = round(10  * (*sensorData / 4094));
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+int ConvertToCurrentRange(int bitInput ,int*sensorData)
+{
+    
+    bool isvalueInRange;
+    int currentValue = 0;
+    if(bitInput = 10)
+    {
+        isvalueInRange =getActualChargeOrDischargeCurrentVaule_10A2D(sensorData,&currentValue); 
+        
+        
+    }
+    else if(bitInput = 12)
+    {
+        isvalueInRange = getActualCurrentvalue_12A2D(sensorData,&currentValue);
+    }
+    else
+    {
+        
+    }
+    return currentValue;
+}
+int getA2DInRangeValue(int* sensorData,int inputBit ,int startRange, int endRange)
+{
+    int RangeValue = sizeof(sensorData);
+    int currentValue[RangeValue] ={0};
+    int noOfValueInRange= 0;
+    
+    
+    for(int i = 0; i< RangeValue ; i++)
+    {
+        currentValue[i] = ConvertToCurrentRange(inputBit ,sensorData[i]);
+    }
+    
+    noOfValueInRange = getNoOfRValuesInRange(&currentValue,startRange ,endRange);
+    return noOfValueInRange;
+}
