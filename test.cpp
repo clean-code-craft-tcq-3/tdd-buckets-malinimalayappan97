eafbff7b8761338af30c4fc1da25e65a226bc605
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 19:15:40 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,8 +4,8 @@
 #include "A2DConverter.h"
 
 TEST_CASE("detect set of values  from  specified range within input array of integers") {
-   int sampleArray[] ={2,3, 3, 5, 4, 10, 11, 12};
-   
+   //int sampleArray[] ={2,3, 3, 5, 4, 10, 11, 12};
+   int sampleArray[] ={1146,1147,4093,4092,2000};
    int startRangeValue = 2;
    int endRangeValue = 5;
    getA2DInRangeValue(sampleArray,12,startRangeValue,endRangeValue);
