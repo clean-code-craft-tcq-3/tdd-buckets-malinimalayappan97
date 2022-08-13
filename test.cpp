bf3b2e8cdc9c3ee270d131ef9013baa62475c825
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 22:09:56 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,18 +3,46 @@
 #include "test/catch.hpp"
 #include "A2DConverter.h"
 
-TEST_CASE("detect set of values  from  specified range within input array of integers") {
-   //int sampleArray[] ={2,3, 3, 5, 4, 10, 11, 12};
+
+TEST_CASE("test1-no definition") {
    int sampleArray[] ={900,650,550,740,640};
    int startRangeValue = 2;
    int endRangeValue = 5;
-   getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,5);
-//    REQUIRE( == 5);
-   
-//    startRangeValue = 10;
-//    endRangeValue = 12;
-   //REQUIRE(getNoOfRValuesInRange(sampleArray,startRangeValue,endRangeValue) == 3);
-   
+   REQUIRE(getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,5)) == 2);
 }
 
+TEST_CASE("test2 2 different consecutive readings") {
+   int sampleArray[] ={900,650,550,740,640,650,640};
+   int startRangeValue = 2;
+   int endRangeValue = 5;
+   REQUIRE(getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,7)) == 4);
+}
+   
+TEST_CASE("test3 - No continuous reading found") {
+   int sampleArray[] ={900,650,550,740,640};
+   int startRangeValue = 2;
+   int endRangeValue = 5;
+   REQUIRE(getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,5)) == 2);
+}
+
+TEST_CASE("test4 - checking if all the sensor values are giving valid current ranges for 12 bit ") {
+   int sampleArray[] ={1146,4094, 2045};
+   int startRangeValue = 2;
+   int endRangeValue = 5;
+   REQUIRE(getA2DInRangeValue(sampleArray,12,startRangeValue,endRangeValue,3)) == 1);
+}
+
+TEST_CASE("test5 - No valid values for 12 bit a2d") {
+   int sampleArray[] ={4095};
+   int startRangeValue = 2;
+   int endRangeValue = 5;
+   REQUIRE(getA2DInRangeValue(sampleArray,12,startRangeValue,endRangeValue,1)) == 0);
+}
+
+TEST_CASE("test5 - No valid values for 10 bit a2d") {
+   int sampleArray[] ={1024};
+   int startRangeValue = 2;
+   int endRangeValue = 5;
+   REQUIRE(getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,1)) == 0);
+}
 
