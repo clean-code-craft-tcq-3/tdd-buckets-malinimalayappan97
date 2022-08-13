52b579e77ca6ed7ea7adf45a79670d0161338a93
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 20:10:45 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 
 TEST_CASE("detect set of values  from  specified range within input array of integers") {
    //int sampleArray[] ={2,3, 3, 5, 4, 10, 11, 12};
-   int sampleArray[] ={900,650,550,700,640};
+   int sampleArray[] ={900,650,550,740,640};
    int startRangeValue = 2;
    int endRangeValue = 5;
    getA2DInRangeValue(sampleArray,10,startRangeValue,endRangeValue,5);
